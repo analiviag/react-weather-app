b4679a0a57d1e33b60dbe299641974bb00caf1cb
analiviag <126291217+analiviag@users.noreply.github.com>
2024-09-06 11:30:58 -0300

    added the footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>React Weather App</h1>
+      <footer>
+        This project was coded by Analivia Costa and is{" "}
+        <a
+          href="https://github.com/analiviag/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
