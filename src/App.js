4e37dde16b1aa59afad3f4b7f90b655f92615eb1
analiviag <126291217+analiviag@users.noreply.github.com>
2024-09-09 12:26:22 -0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <footer>
-        This project was coded by Analivia Costa and is{" "}
-        <a
-          href="https://github.com/analiviag/react-weather-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>React Weather App</h1>
+        <footer>
+          This project was coded by Analivia Costa and is{" "}
+          <a
+            href="https://github.com/analiviag/react-weather-app"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
