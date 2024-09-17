0e079c0eca76a6dbeeff8fbbb9ab409c86aae804
analiviag <126291217+analiviag@users.noreply.github.com>
2024-09-17 16:23:37 -0300

    added axios
    and some data to the project, missing a few data still

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Vancouver" />
 
         <footer>
           This project was coded by Analivia Costa and is{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,49 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function displayResponse(response) {
+    console.log(response);
+    setWeatherData({
+      ready: true,
+      temperature: Math.round(response.data.temperature.current),
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
+  }
+
+  function search() {
+    const apiKey = "bba264o26c1cdt003d73631d7a2a1f27";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search for a city"
+                size={80}
+                autoFocus="on"
+                className="form-control"
+              ></input>
+            </div>
+            <div className="col-3">
+              <button className="btn btn-primary">Search</button>
+            </div>
+          </div>
+        </form>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Search for a city"
-              size={80}
-              autoFocus="on"
-            ></input>
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="Cloudy"
+              height={80}
+              width={80}
+            ></img>
+            <span className="temperature">{weatherData.temperature}</span>
+            <span className="unit-C">°C |</span>
+            <span className="unit-F">°F</span>
+            <ul>
+              <li>Precipitation: 3%</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
           </div>
-          <div className="col-3">
-            <button className="btn btn-primary">Search</button>
+          <div className="col-6">
+            <h1>{weatherData.city}</h1>
+            <ul>
+              <li>Monday 9:00</li>
+              <li>{weatherData.description}</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="Cloudy"
-            height={80}
-            width={80}
-          ></img>
-          <span className="temperature">17</span>
-          <span className="unit-C">°C |</span>
-          <span className="unit-F">°F</span>
-          <ul>
-            <li>Precipitation: 3%</li>
-            <li>Humidity: 84%</li>
-            <li>Wind: 5 km/h</li>
-          </ul>
-        </div>
-        <div className="col-6">
-          <h1>Vancouver</h1>
-          <ul>
-            <li>Monday 9:00</li>
-            <li>Cloudy</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
